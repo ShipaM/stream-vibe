fa8e48e7e0ba846e0d1d537c329e40dd88fd1cd7
Maksym Shypytsia <30442574+ShipaM@users.noreply.github.com>
2025-08-09 15:38:29 +0300

    feat(add-styles): add styles for header

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,10 +1,34 @@
 import { Logo } from "@/components/Logo"
 import "./Header.scss"
-const Header = () => {
+import classNames from "classnames"
+const Header = ({ url }) => {
+  console.log(url)
+  const menuItems = [
+    { name: "Home", href: "/" },
+    { name: "Movies & Shows", href: "/movies" },
+    { name: "Support", href: "/support" },
+    { name: "Contact", href: "/subscriptions" },
+  ]
   return (
     <header className="header">
       <div className="header__inner container">
-        <Logo loading="eager" />
+        <Logo className="header__logo" loading="eager" />
+        <nav className="header__menu">
+          <ul className="header__menu-list">
+            {menuItems.map(({ name, href }) => (
+              <li key={name} className="header__menu-item">
+                <a
+                  href={href}
+                  className={classNames("header__menu-link", {
+                    "is-active": url === href,
+                  })}
+                >
+                  {name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
     </header>
   )
