96ee3453db269bff15114e1561c0978b22a8142b
Maksym Shypytsia <30442574+ShipaM@users.noreply.github.com>
2025-08-10 13:37:20 +0300

    feat(add styles for burger menu): add styles for burger-menu

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -15,39 +15,42 @@ const Header = ({ url }) => {
     <header className="header">
       <div className="header__inner container">
         <Logo className="header__logo" loading="eager" />
-        <nav className="header__menu">
-          <ul className="header__menu-list">
-            {menuItems.map(({ name, href }) => (
-              <li key={name} className="header__menu-item">
-                <a
-                  href={href}
-                  className={classNames("header__menu-link", {
-                    "is-active": url === href,
-                  })}
-                >
-                  {name}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <div className="header__actions">
-          <Button
-            className="header__button"
-            label="Search"
-            isLabelHidden
-            iconName={"Search"}
-            mode="transparent"
-          />
-          <Button
-            href="/"
-            label="Notifications"
-            isLabelHidden
-            iconName={"Notifications"}
-            mode="transparent"
-          />
-        </div>
-        <BurgerButton className="header__burger-button" />
+        <dialog className="header__overlay-menu-dialog">
+          <nav className="header__menu">
+            <ul className="header__menu-list">
+              {menuItems.map(({ name, href }) => (
+                <li key={name} className="header__menu-item">
+                  <a
+                    href={href}
+                    className={classNames("header__menu-link", {
+                      "is-active": url === href,
+                    })}
+                  >
+                    {name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <div className="header__actions">
+            <Button
+              className="header__button"
+              label="Search"
+              isLabelHidden
+              iconName={"Search"}
+              mode="transparent"
+            />
+            <Button
+              href="/"
+              label="Notifications"
+              isLabelHidden
+              iconName={"Notifications"}
+              mode="transparent"
+            />
+          </div>
+        </dialog>
+
+        <BurgerButton className="header__burger-button visible-tablet" />
       </div>
     </header>
   )
